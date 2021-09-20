9cfe163536870a8cc852cf17b22996fb96708ead
Xander Vertegaal <73882506+XanderVertegaal@users.noreply.github.com>
2021-09-20 15:33:49 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log('Hallo Winc Academy studenten');
+// Dit is een grote som (Deze comment verschijnt niet in de console.)
+/* Alles in deze block-comment verschijnt niet in de console
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+*/
